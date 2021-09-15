98e9309c833f4cde8babe6fb302644b016453033
JamaCodes <jama.avwork@gmail.com>
2021-09-15 16:22:46 -0500

    css drpdwn

--- scripts/SelectorCards.js
+++ scripts/SelectorCards.js
@@ -1,7 +1,7 @@
 export const ParkSelectorCard = (parkArray) => {
   let output = `<section class="parkSelector"><select name="parks" class="dropdown" id="parkDropdown"><option value="default">Select Park</option>`;
   for (const park of parkArray) {
-    output += `<option class="parkitem" value="${park.fullName}">${park.fullName}</option>`;
+    output += `<option class="dropdownitem" value="${park.fullName}">${park.fullName}</option>`;
   }
   output += `</select></section>`;
   return output;
@@ -10,7 +10,7 @@ export const ParkSelectorCard = (parkArray) => {
 export const BizSelectorCard = (bizArray) => {
   let output = `<section class="bizSelector"><select name="bizarraries" class="dropdown" id="bizDropdown"><option value="default">Select Biz</option>`;
   for (const biz of bizArray) {
-    output += `<option value="${biz.name}">${biz.name}</option>`;
+    output += `<option class="dropdownitem" value="${biz.name}">${biz.name}</option>`;
   }
   output += `</select></section>`;
   return output;
@@ -19,7 +19,7 @@ export const BizSelectorCard = (bizArray) => {
 export const EaterySelectorCard = (eateryArray) => {
   let output = `<section class="eaterySelector"><select name="eateries" class="dropdown" id="eateryDropdown"><option value="default">Select Eatery</option>`;
   for (const eatery of eateryArray) {
-    output += `<option value="${eatery.businessName}">${eatery.businessName}</option>`;
+    output += `<option class="dropdownitem" value="${eatery.businessName}">${eatery.businessName}</option>`;
   }
   output += `</select></section>`;
   return output;
