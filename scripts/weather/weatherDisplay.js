23be169385c046be152a9d703e6e654dc51c4c26
joshuastewart417 <joshua.stewart780@gmail.com>
2021-09-15 09:39:05 -0500

    commit to merge dropdown display functionality

--- scripts/weather/weatherDisplay.js
+++ scripts/weather/weatherDisplay.js
@@ -1,40 +1,18 @@
 import { getWeather } from "./WeatherDataManager.js";
 import { weatherListing } from "./weatherList.js";
 
+export const callApi = () => {
+  getWeather().then((data) => {
+    const display = document.querySelector(".display");
 
-export const callApi = () =>
-{
+    let allHtml = "";
 
+    const allData = data.list;
 
-    getWeather().then(data =>
-        
-        { 
-          
-            
+    for (const date of allData) {
+      allHtml += weatherListing(date, data.city.name);
+    }
 
-            const display = document.querySelector('.display')
-
-            let allHtml = ""
-
-            const allData = data.list
-            
-            for(const date of allData)
-
-            {
-
-                allHtml+= weatherListing(date, data.city.name)
-                console.log(date)
-                
-            }
-
-        
-
-            display.innerHTML += allHtml;
-
-
-        }
-
-            
-   
-
-    )}
+    display.innerHTML += allHtml;
+  });
+};
