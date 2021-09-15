8836fe28ec0bf14bf1b6b95a54bd1d8ef1cca464
joshuastewart417 <joshua.stewart780@gmail.com>
2021-09-15 10:00:56 -0500

    updating PR

--- scripts/weather/weatherDisplay.js
+++ scripts/weather/weatherDisplay.js
@@ -10,19 +10,8 @@ export const callApi = () => {
     const allData = data.list;
 
     for (const date of allData) {
-      allHtml += weatherListing(date, data.city.name);
-    }
-
-    const display = document.querySelector(".display");
-
-    let allHtml = "";
-
-    const allData = data.list;
-
-    for (const date of allData) {
       if (date.dt_txt.split(" ")[1] == "21:00:00") {
         allHtml += weatherListing(date, data.city.name);
-        console.log(date);
       }
     }
 
