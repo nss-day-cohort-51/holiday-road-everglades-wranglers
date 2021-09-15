00d47ac69b4157bb6130eb26e7f9151640655177
joshuastewart417 <joshua.stewart780@gmail.com>
2021-09-15 14:32:04 -0500

    updated styling on trip preview cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,18 @@
 import { USMap } from "./map/Map.js";
 import { getParks } from "./parks/ParkDataManager.js";
-import { ParkSelectorCard,EaterySelectorCard,BizSelectorCard } from "./SelectorCards.js";
-import { getEateries } from "./eateries/EateryDataManager.js"
-import { getAttractions } from "./attractions/AttractionDataManager.js"
-import { ParkPreviewCard, BizPreviewCard, EateryPreviewCard } from "./TripPreviewCards.js";
+import {
+  ParkSelectorCard,
+  EaterySelectorCard,
+  BizSelectorCard,
+} from "./SelectorCards.js";
+import { getEateries } from "./eateries/EateryDataManager.js";
+import { getAttractions } from "./attractions/AttractionDataManager.js";
+import {
+  ParkPreviewCard,
+  ParkDetailsCard,
+  BizPreviewCard,
+  EateryPreviewCard,
+} from "./TripPreviewCards.js";
 
 // ===============================================================================
 
@@ -12,59 +21,80 @@ const applicationElement = document.querySelector(".mapSection");
 
 // =============================event listeners===================================
 
-let parkData = null
-let bizData = null
-let eateryData = null
+let parkData = null;
+let bizData = null;
+let eateryData = null;
 
 applicationElement.addEventListener("click", (event) => {
-  parkElement.innerHTML = "<h2>select a park bizzarrie and eatery</h2>"
+  parkElement.innerHTML = "<h2>select a park bizzarrie and eatery</h2>";
   if (event.target.id.length === 2) {
     getParks(event.target.id).then((park) => {
-      parkElement.innerHTML += ParkSelectorCard(park.data); 
-      parkData = park.data
+      parkElement.innerHTML += ParkSelectorCard(park.data);
+      parkData = park.data;
     });
-     getEateries(event.target.id).then((eatery) => {
+    getEateries(event.target.id).then((eatery) => {
       parkElement.innerHTML += EaterySelectorCard(eatery);
-      eateryData = eatery
+      eateryData = eatery;
     });
     getAttractions(event.target.id).then((attractions) => {
       parkElement.innerHTML += BizSelectorCard(attractions);
-      bizData = attractions
+      bizData = attractions;
     });
   }
 });
 
-document.getElementsByClassName("tripSelection")[0].addEventListener('click', function(event) {
-  if (event.target && event.target.className === "dropdown" && event.target.id.endsWith("Dropdown")) {
-    if (event.target.selectedIndex > 0) {
-      if (event.target.id === "parkDropdown") {
-        ShowParkPreview(event, parkData);
-      } else if (event.target.id === "bizDropdown") {
-        ShowBizPreview(event, bizData);
-      } else if  (event.target.id === "eateryDropdown") {
-        ShowEateryPreview(event, eateryData);
+document
+  .getElementsByClassName("tripSelection")[0]
+  .addEventListener("click", function (event) {
+    if (
+      event.target &&
+      event.target.className === "dropdown" &&
+      event.target.id.endsWith("Dropdown")
+    ) {
+      if (event.target.selectedIndex > 0) {
+        if (event.target.id === "parkDropdown") {
+          ShowParkPreview(event, parkData);
+        } else if (event.target.id === "bizDropdown") {
+          ShowBizPreview(event, bizData);
+        } else if (event.target.id === "eateryDropdown") {
+          ShowEateryPreview(event, eateryData);
+        }
       }
     }
-  }
-})
+  });
+
+const ShowParkDetails = (event, data) => {
+  let parkDesc = event.target.options[event.target.selectedIndex].description;
+  document.querySelector(".previewDetails").innerHTML +=
+    ParkDetailsCard(parkDesc);
+};
 
 const ShowBizPreview = (event, data) => {
-  let parkName = event.target.options[event.target.selectedIndex].text
-  let parkImage = data[event.target.selectedIndex].description
-  document.querySelector(".previewCards").innerHTML += BizPreviewCard(parkName, parkImage)
-}
+  let parkName = event.target.options[event.target.selectedIndex].text;
+  let parkImage = data[event.target.selectedIndex].description;
+  document.querySelector(".previewCards").innerHTML += BizPreviewCard(
+    parkName,
+    parkImage
+  );
+};
 
 const ShowEateryPreview = (event, data) => {
-  let parkName = event.target.options[event.target.selectedIndex].text
-  let parkImage = data[event.target.selectedIndex].description
-  document.querySelector(".previewCards").innerHTML += EateryPreviewCard(parkName, parkImage)
-}
+  let parkName = event.target.options[event.target.selectedIndex].text;
+  let parkImage = data[event.target.selectedIndex].description;
+  document.querySelector(".previewCards").innerHTML += EateryPreviewCard(
+    parkName,
+    parkImage
+  );
+};
 
 const ShowParkPreview = (event, data) => {
-  let parkName = event.target.options[event.target.selectedIndex].text
-  let parkImage = data[event.target.selectedIndex - 1].images[0].url
-  document.querySelector(".previewCards").innerHTML += ParkPreviewCard(parkName, parkImage)
-}
+  let parkName = event.target.options[event.target.selectedIndex].text;
+  let parkImage = data[event.target.selectedIndex - 1].images[0].url;
+  document.querySelector(".previewCards").innerHTML += ParkPreviewCard(
+    parkName,
+    parkImage
+  );
+};
 
 // ===========================map function========================================
 
