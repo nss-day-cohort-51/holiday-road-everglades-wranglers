5e2aa1bb65e036295b6bc5b385fdd4dbe3e1ef68
joshuastewart417 <joshua.stewart780@gmail.com>
2021-09-16 15:35:58 -0500

    finalized styling for trip preview dropdowns

--- scripts/TripPreviewCards.js
+++ scripts/TripPreviewCards.js
@@ -1,7 +1,7 @@
 export const ParkPreviewCard = (parkName, image) => {
   return `
     <section class="parkPreview">
-        <h3>National Park: </h3>
+        <h3>National Park : </h3>
         <h3>${parkName}</h3>
         <section class="modifyButtons">
             <button class="parkDetailsBtn" id="detailsBtn_${parkName}">Details</button>
@@ -10,16 +10,15 @@ export const ParkPreviewCard = (parkName, image) => {
 };
 
 export const ParkDetailsCard = (parkImage, parkDesc) => {
-  return `<div class="parkinfo_wrapper">
-            <img class="park_image" src="${parkImage}" alt="parkImage" />
+  return `<img class="park_image" src="${parkImage}" alt="parkImage" />
             <div class="park_description">${parkDesc}</div>
-          </div>`;
+          `;
 };
 
 export const BizPreviewCard = (bizName, desc) => {
   return `
     <section class="bizPreview">
-        <h3>"Bizarrerie"(Attraction): </h3> 
+        <h3>"Bizarrerie" : </h3> 
         <h3>${bizName}</h3>
         <section class="modifyButtons">
             <button class="bizDetailsBtn" id="detailsBtn_${bizName}">Details</button>
@@ -35,7 +34,7 @@ export const BizDetailsCard = (bizDesc) => {
 export const EateryPreviewCard = (eateryName, desc) => {
   return `
     <section class="eateryPreview">
-        <h3>Eatery: </h3>
+        <h3>Eatery : </h3>
         <h3>${eateryName}</h3>
         <section class="modifyButtons">
             <button class="eatDetailsBtn" id="detailsBtn_${eateryName}">Details</button>
